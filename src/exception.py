02bda66c18baa862c805445d259d5424e33c6554
NandiniVerma994 <dev.nandiniv@gmail.com>
2024-07-02 23:50:54 +0530

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -22,5 +22,25 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
+# if __name__ == "__main__":
 
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by Zero")
+#         raise CustomException(e,sys)
+    
 
+'''This Python code snippet is designed to handle exceptions in 
+a structured way, providing detailed error messages that include 
+the script name, line number, and error message.
+ error_message_detail function: This function is designed to format
+   and return a detailed error message when an exception occurs. It 
+   takes two parameters: error, which is the exception object, and 
+   error_detail, which is expected to be a module that presumably 
+   has an exe_info() method (likely a typo for sys.exc_info() from
+     the sys module). The function extracts the traceback object
+       from error_detail, then retrieves the filename and the line 
+       number where the exception occurred. It formats these details
+         into a string that includes the script name, line number, 
+         and the error message, and returns this string.'''
--- src/logger.py
+++ src/logger.py
@@ -14,3 +14,23 @@ logging.basicConfig(
     level = logging.INFO,
 )
 
+'''This Python code snippet is designed to create a logging system 
+that writes logs to a file. Here's a brief explanation of its components:
+
+LOG_FILE: Generates a log file name based on the current date and
+ time, ensuring each log file has a unique name.
+logs_path: Constructs a path for the log files by joining the 
+current working directory with a "logs" folder and the log file
+ name. This is intended to create a directory path, but there's
+   a mistake in its implementation.
+os.makedirs(logs_path, exist_ok=True): Attempts to create the 
+directory path for the logs. The exist_ok=True parameter means it
+ won't throw an error if the directory already exists. However, 
+ this line is incorrectly trying to create a directory with the
+   same name as the log file.
+LOG_FILE_PATH: Intended to define the full path to the log file,
+ but due to the earlier mistake, it redundantly joins the already
+   complete logs_path with LOG_FILE, leading to an incorrect path.
+logging.basicConfig(...): Configures the root logger to 
+write logs to the file specified by LOG_FILE_PATH, with a
+ specific format for log messages and a logging level of INFO.'''
